7f5139beec82efabb2e19b32cc1f6c9a1cd7f50d
Andrei <aiurciuc@gmail.com>
2025-05-24 23:17:55 +0300

    Add a header

--- src/components/shared/Layout/Header/Header.jsx
+++ src/components/shared/Layout/Header/Header.jsx
@@ -1,9 +1,19 @@
-import styles from './Header.module.scss';
+import styles from "./Header.module.scss";
+import { Link } from "react-router";
 
 function Header() {
-  return <header className={styles.header} >
-    <h1 className={styles.title}>Car Rental</h1>
-  </header>;
+
+  return (
+    <header className={styles.header}>
+       <Link to="/" aria-label="Navigate to home">
+        <img
+          className={styles.logo}
+          src="/src/assets/logo/partner.svg"
+          alt="Car Rental Logo"
+        />
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
